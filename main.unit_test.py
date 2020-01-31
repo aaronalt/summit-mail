41ae8817a2f1213c64b0b294b28699c7b965a4ca
aaronalt <aaronalt07@gmail.com>
2020-01-31 14:39:30 +0300

    added tmp file to test

--- main.unit_test.py
+++ main.unit_test.py
@@ -1,12 +1,28 @@
 import unittest
 from main import SummitMail
 import os
+import tempfile
 
 
 class EmailTest(unittest.TestCase):
     def setUp(self):
         sender = os.getenv('SENDER_EMAIL')
         pw = os.getenv('SENDER_EMAIL_PASSWORD')
+        self.fake_csv = tempfile.TemporaryFile('w+')
+        # create fake csv
+        with self.fake_csv as csv:
+            print("Creating fake csv...")
+            headers = ['name', 'category', 'country', 'website', 'email', 'stage', 'status', 'contact date',
+                       'contact method', 'result', 'error result', 'note']
+            header_line = ','.join(i for i in headers)
+            csv.write(header_line + "\n")
+            tester = ['Aaron', 'Security', 'U.S.A', 'github.com/aaronalt', 'aaron@mango-byte.com', 'Alpha', '', '', '',
+                      '', '', '', '']
+            tester_line = ','.join(i for i in tester)
+            csv.write(tester_line)
+            print("...done.")
+            csv.seek(0)
+            print(csv.read())
         self.email = SummitMail(sender, pw, "test.csv", "rise", "Summit 2020")
 
     def test_build_email(self):
@@ -17,6 +33,16 @@ class EmailTest(unittest.TestCase):
         self.assertFalse(self.email.sender_email)
         self.assertTrue(os.path.exists('rise.txt'))
         self.assertTrue(os.path.exists('rise.html'))
+
+    def test_filter_emails(self):
+        self.assertFalse(self.email.client_list)
+        self.email.filter_emails()
+        self.assertTrue(self.email.client_list)
+        # there is no 'append_list' at this point
+        # should make new file that deletes after testing
+
+    def tearDown(self):
+        self.fake_csv.close()
 
 
 if __name__ == '__main__':
