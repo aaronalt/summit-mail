8384476efaef66d508d5341c8435fa752111e1f6
aaronalt <aaronalt07@gmail.com>
2020-05-20 19:51:27 +0300

    removed unecessaries

--- main.py
+++ main.py
@@ -4,17 +4,6 @@ from ClientFilter import ClientFilter
 
 class SummitMail:
 
-    # connect with Airtable API
-    """
-    session = requests.Session()
-    base_id = 'appTpBROWIBsmE7FD'  # airtable base id
-    table_is = 'tblMUdQtfnQyo36JH'
-    api_key = os.getenv('AIRTABLE_API_KEY')
-    """
-
-    # subject = input("    email subject: ")
-    # html_txt = input("    html filename: ")
-    # csv_file = input("    csv filename: ") + ".csv"
     email = Email("App Development Support", "Inputs/contact_new_clients")
     email.send_external("New Contacts-Grid view.csv")
 
