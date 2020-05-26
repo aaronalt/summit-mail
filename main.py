f459a2eb4cba946e77c3a47934e8e09038b2b856
aaronalt <aaronalt07@gmail.com>
2020-05-26 13:21:01 +0300

    added dotenv library

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
+
 from Email import Email
 import requests
 import os
+import datetime
 from Client import Client
 from airtable import Airtable
 
+from dotenv import load_dotenv
+load_dotenv()
+
 
 class SummitMail:
-    new_contacts = Airtable('appTpBROWIBsmE7FD', "New Contacts", 'keyAlrM1hXFqzhEnm')
-    countries_table = Airtable('appTpBROWIBsmE7FD', "Countries", 'keyAlrM1hXFqzhEnm')
+    d = datetime.datetime.now()
+    date = f'{d.day}/{d.month}/{d.year}'
+
+    new_contacts = Airtable('appTpBROWIBsmE7FD', "New Contacts", api_key=os.getenv("AIRTABLE_API_KEY"))
+    countries_table = Airtable('appTpBROWIBsmE7FD', "Countries", api_key=os.getenv("AIRTABLE_API_KEY"))
     all_countries = countries_table.get_all()
     client_objects = []
-    print(all_countries)
+
     for id in new_contacts.get_all(formula="{status}=''", max_records=25):
+        # update country field with string from record id
         country = ''
         for i in all_countries:
             if i['id'] == id['fields']['country'][0]:
                 country = i['fields']['Name']
                 break
-        client_objects.append(Client(id['fields']['name'].strip(), country,
-                                     id['fields']['website'],
-                                     id['fields']['email'].strip()))
+
+        new_client = Client(id['fields']['name'].strip(), country,
+                            id['fields']['website'],
+                            id['fields']['email'].strip())
+        client_objects.append(new_client)
+
+        record = new_contacts.match('name', new_client.name)
+        fields = {'name': new_client.name, 'country': new_client.country, 'website': new_client.website,
+                  'email': new_client.email, 'status': 'Contacted', 'contact date': date, 'contact method': 'Email',
+                  'source': 'goodfirms.co', 'result': 'No response'}
+        new_contacts.update(record['id'], fields, typecast=True)
+
     email = Email("App Development Support", "Inputs/contact_new_clients")
     email.send_external(client_objects)
 
