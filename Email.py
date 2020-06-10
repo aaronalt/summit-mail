04a5c624417b9c66a9d1bfc23955faa000a85d66
aaronalt <aaronalt07@gmail.com>
2020-06-10 13:15:24 +0300

    refactored

--- Email.py
+++ Email.py
@@ -10,13 +10,14 @@ from ClientFilter import ClientFilter
 
 class Email:
 
-    def __init__(self, subject, html_txt, cfg_name):
+    def __init__(self, subject, files_source, cfg_name):
         self.subject = subject
-        self.html_txt = f'{html_txt}.txt'
+        self.files_source = f'{files_source}.txt'
 
-        self.cfg_name = cfg_name
+        self.cfg_name = f'Cfg/{cfg_name}.ini'
+        print(self.cfg_name)
         self.cfg = configparser.ConfigParser()
-        self.cfg.read(os.path.join('Cfg/', self.cfg_name))
+        print(self.cfg.read(self.cfg_name))
         self.sender_email = self.cfg['settings']['sender_email']
         self.password = self.cfg['settings']['sender_email_password']
         self.test_email = self.cfg['settings']['test_email']
@@ -27,20 +28,20 @@ class Email:
         message = self.message
         message["Subject"] = self.subject
         message["From"] = self.sender_email
-        with open(self.html_txt, "r") as t:
+        with open(self.files_source, "r") as t:
             text = t.read()
-        with open(self.html_txt, "r") as h:
+        with open(self.files_source, "r") as h:
             html = h.read()
         part1 = MIMEText(text, "plain")
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
 
-    def send_test_once(self, send_to):
+    def send_test_once(self):
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=self.context) as server:
             server.login(self.sender_email, self.password)
             server.sendmail(
-                self.sender_email, send_to, self.message.as_string()
+                self.sender_email, self.test_email, self.message.as_string()
             )
         return "Testing done..."
 
